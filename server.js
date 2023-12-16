d49966f543c2a221d590f90abce0eb89a5a6fa48
izzy-2023 <izzyzinxhirija@gmail.com>
2023-12-16 16:24:21 -0500

    setting up my express server

---
+++ server.js
@@ -0,0 +1,39 @@
+//*************************** */
+// DEPENDENCIES
+// Brings things from libraries and other files
+//*************************** */
+
+//import the express library
+const express = require("express")
+
+//*************************** */
+// Create Express Application Object
+// This is the center of our express universe
+//**************************** */
+
+// express(): returns an express application object
+
+const app = express()
+
+//*****************************
+// ROUTES
+// Which function should run for differnt (method/url) pairs
+// ****************************
+
+app.get("/", (req, res) => {
+    // res.send(something) will send the response based on the input
+    // an html string: will send back as html
+    // js array or object: will send back json
+    // string: sends back as text
+    res.send("<h1>Welcome to the Gitpub App!</h1>")
+})
+
+
+// *****************************
+// TURNING ON SERVER LISTENER
+// WILL TELL OUR APP TO LISTEN FOR REQUESTS
+// ON A CERTAIN PORT
+// *****************************
+// app.listen(PORT, FUNCTION)
+// The function will run after the server turns on
+app.listen(3000, () => {console.log("server is listening on port 3000")})
